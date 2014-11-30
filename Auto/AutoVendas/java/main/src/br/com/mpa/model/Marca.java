ea12378c0c8760601f2e1bff348d8e1fd423a282
Henrique Eichler <henrique@192.168.0.100>
2014-11-30 13:54:16 -0200

    Entity classes added.

---
+++ Auto/AutoVendas/java/main/src/br/com/mpa/model/Marca.java
@@ -0,0 +1,58 @@
+package br.com.mpa.model;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Marca {
+
+	@Id @GeneratedValue
+	private Long id;
+
+	@Column(nullable=false, length=250, unique=true)
+	private String nome;
+
+	@Lob
+	private byte[] logo;
+
+	@OneToMany(mappedBy="marca")
+	private List<Modelo> modelos;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public byte[] getLogo() {
+		return logo;
+	}
+
+	public void setLogo(byte[] logo) {
+		this.logo = logo;
+	}
+
+	public List<Modelo> getModelos() {
+		return modelos;
+	}
+
+	public void setModelos(List<Modelo> modelos) {
+		this.modelos = modelos;
+	}
+}
---
+++ Auto/AutoVendas/java/main/src/br/com/mpa/model/Modelo.java
@@ -0,0 +1,45 @@
+package br.com.mpa.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Modelo {
+
+	@Id @GeneratedValue
+	private Long id;
+
+	@Column(nullable=false, length=250, unique=true)
+	private String nome;
+
+	@ManyToOne
+	private Marca marca;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Marca getMarca() {
+		return marca;
+	}
+
+	public void setMarca(Marca marca) {
+		this.marca = marca;
+	}
+	
+}
